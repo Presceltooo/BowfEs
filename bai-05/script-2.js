3bf5979594f43a26cc1f4dd3852ea3a8a29049a3
DTC174 <a01672034597@gmail.com>
2024-03-06 09:52:02 +0700

    Update file script-2 moi

---
+++ bai-05/script-2.js
@@ -0,0 +1,12 @@
+//05. 1 số hàm built-in
+
+// alert("Xin chào các bạn");
+
+// var result = prompt("Bạn đã trên 18 tuổi ch?");
+// console.log(result);
+// console.warn(result);
+// console.error(result);
+
+setTimeout(function() {
+  console.log("đã đc 5 giây")
+}, 5000);
