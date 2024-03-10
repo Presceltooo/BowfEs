6a0e2e3b0831e9eaf7e407aa70f531ca108a3e90
DTC174 <a01672034597@gmail.com>
2024-03-10 22:32:46 +0700

    tạm xong phan 1 Function

---
+++ bai-07/script.js
@@ -0,0 +1,36 @@
+// 1.1. Function
+// function tong(a, b) {
+//   return a + b;
+// }
+// var ketQua = tong(5, 6);
+// console.log(ketQua);
+// tuy nhiên nhược điểm là chỉ cộng đc 2 phần tử, nhưng nhu cầu mình là nhiều phần tử
+
+//1.2. Declaration Function (Hàm định nghĩa)
+// có tính hoisting, đc gọi hàm trc khai báo hàm đó
+// có đồi tượng arguments
+// function tong() {
+//   // console.log(arguments); đối tượng arguments là 1 đối tượng đặc biệt trong js, nó chứa tất cả các tham số truyền vào hàm và đặc biệt là nó có sẵn
+//   let tong = 0;
+//   for (const item of arguments) {
+//     tong += item;
+//   }
+//   return tong;
+// }
+// var ketQua = tong(5, 6, 7, 8, 9);
+// console.log(ketQua);
+
+// Expression Function (Hàm biểu thức)
+// k có tính hoisting, nên k thể gọi hàm trc khai báo hàm đó
+// cũng có đối tượng arguments
+ var tong = function() {
+  let tong = 0;
+  for (const item of arguments) {
+    tong += item;
+  }
+  return tong;
+}
+var ketQua = tong(5, 6, 7, 8, 9);
+console.log(ketQua);
+
+
