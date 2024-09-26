1cb26d5d99ae0858d1157ec0d52a545fefb8e97b
DTC174 <a01672034597@gmail.com>
2024-09-26 21:06:59 +0700

    Hoàn thành bài 11 (scope,hoisting,this, JSON)

--- bai-11/script.js
+++ bai-11/script.js
@@ -118,15 +118,135 @@
   // Không tồn tại đối tượng this
   // nếu dùng thì nó trả ra this của window
 
-  const button = document.querySelector("#button");
+  // const button = document.querySelector("#button");
 
-  button.addEventListener("click", () => {
-    // console.log(this); // trả ra this của window
-    console.log(button); // vì vậy ta có thể thây bằng việc dùng luôn biến buttonthay vì this
-  });
+  // button.addEventListener("click", () => {
+  //   // console.log(this); // trả ra this của window
+  //   console.log(button); // vì vậy ta có thể thây bằng việc dùng luôn biến buttonthay vì this
+  // });
 
 // 4. Module: giống tính chất đóng gói trong OOP (tái sử dụng code 1 cách hiệu quả)
 
+// 5. JSON: là một định dạng dữ liệu được lưu dưới dạng chuỗi.
+// chỉ cho phép kiểu dữ liệu cơ bản: number, boolen, string, array, object, null
+// không cho phép: function, undefined, date, biến, ...
+// Trường hợp: Giá trị của JSON là Object thì:
+  // có các cặp key/value
+  // key đặt trong dấu nháy kép
+  // k có dấu phẩy ở cặp key/value cuối cùng
+
+  // const objectJS = {
+  //   name: "Duy Tung",
+  //   age: 21,
+  //   email: "abc@gmail.com",
+  //   phone: "0123456789",
+  // };
+
+  // const objectJSON = `{
+  //   "name": "Duy Tung",
+  //   "age": 21,
+  //   "email": "abc@gmail.com",
+  //   "phone": "0123456789"
+  // }
+  // `;
+  // // trong objectJSON thì k thể thêm hàm ở trong như objectJS
+
+  // VD1: JSON có giá trị là Object
+    // const objectJSON = `{
+    // "name": "Duy Tung",
+    // "age": 21,
+    // "email": "abc@gmail.com",
+    // "phone": "0123456789"
+    // }
+    // `;
+    // console.log(typeof objectJSON); // string
+    // console.log(objectJSON); // in ra chuỗi JSON
+
+    // // Chuyển từ JSON sang JS
+    // const objectJS = JSON.parse(objectJSON);
+    // console.log(objectJS); // in ra object
+
+    // // Chuyển từ JS sang JSON
+    // const objectJSON2 = JSON.stringify(objectJS);
+    // console.log(objectJSON2); // in ra chuỗi JSON
+  
+    // lí do lại có JSON: là string lên dễ gửi qua API, dễ lưu trữ, dễ truyền dữ liệu
+
+
+  // VD2: JSON có giá trị là Number
+    // var numberJSON = `10`;
+
+    // console.log(numberJSON);
+
+    // var numberJS = parseInt(numberJSON);
+    // điểm yếu khi sử dụng parseInt là nếu chuỗi k phải số mà là `ádasf` thì khi ấy nó sẽ trả về NaN
+    // vì vậy lên dùng JSON.parse
+
+    // chuyen JSON qua JS
+    // var numberJS = JSON.parse(numberJSON);
+    // console.log(numberJS);
+    // //
+
+    // // chuyen JS qua JSON
+    // const numberJSON2 = JSON.stringify(numberJS);
+    // console.log(numberJSON2);
+
+  // VD3: JSON có giá trị là String
+    // const stringJSON = `"abc"`;
+
+    // // chuyen JSON qua JS
+    // var stringJS = JSON.parse(stringJSON);
+    // console.log(stringJS);
+
+    // // chuyen JS qua JSON
+    // const stringJSON2 = JSON.stringify(stringJS);
+    // console.log(stringJSON2);
+
+  // VD4: JSON có giá trị là Boolean
+    // const booleanJSON = `true`;
+
+    // // chuyen JSON qua JS
+    // var booleanJS = JSON.parse(booleanJSON);
+    // console.log(booleanJS);
+    
+    // // chuyen JS qua JSON
+    // const booleanJSON2 = JSON.stringify(booleanJS);
+    // console.log(booleanJSON2);
+
+  // VD5: JSON có giá trị là Array
+    // cấu trúc của Array nếu quên
+    // const arrayJSON = `[{
+    //   "name": "Duy Tung",
+    //   "age": 21,
+    //   "phone": "0123456789"
+    // },
+    // {
+    //   "name": "Do Duy Tung",
+    //   "age": 21,
+    //   "phone": "0123456788"
+    // }
+    // ]`;
+
+    // // chuyen JSON qua JS
+    // var arrayJS = JSON.parse(arrayJSON);
+    // console.log(arrayJS);
+
+    // // chuyen JS qua JSON
+    // const arrayJSON2 = JSON.stringify(arrayJS);
+    // console.log(arrayJSON2);
+
+
+  // VD6: JSON có giá trị là Null
+    const nullJSON = `null`;
+
+    // chuyen JSON qua JS
+    var nullJS = JSON.parse(nullJSON);
+    console.log(nullJS);
+    //
+
+    // chuyen JS qua JSON
+    const nullJSON2 = JSON.stringify(nullJS);
+    console.log(nullJSON2);
 
 
 
