b37f83bf2e164aae306af441bdc0395e41a0cfbd
DTC174 <a01672034597@gmail.com>
2024-10-02 00:20:08 +0700

    Tạm được

--- bai-14/vi-du-01/index.js
+++ bai-14/vi-du-01/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 // md để gọi thử viện express
 const app = express()
+// app có nghĩa là toàn bộ chương trình của ta
 const port = 3000
 
 app.set('views', './views'); // tim den thu muc views de render ra html
@@ -10,6 +11,8 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
+  // "/" mang như truy cập vào router, trang chủ
+
   // const listUser = [
   //   {
   //     fullName: "Nguyen Van A",
@@ -22,12 +25,19 @@ app.get('/', (req, res) => {
   // ];
   // console.log(listUser);
   // console.log("Okay");
+
+// Template engine: là công cụ để tách các mã HTML từ đó có thể tái sử dụng các đoạn HTML đó.
+
   res.render('index.pug', {
     title: "Trang chủ",
     message: "Xin chào mọi người"
     // ngoài việc có những câu lệnh ở .pug thì ta có thể thêm 1 số nhưng data động ngay tại đây
   }) // do da co chieu vao thu muc views nen k can ghi can than chi dan
 });
+
+// MPA: mutiple Page Application: là load lại toàn bộ trang hay sever sẽ render lại toàn bộ trang (html)xg mới gửi cho SPA => tốn nhiều thời gian
+// SPA: Single Page Application: là chỉ load 1 phần của của trang, sử dụng qua API => giảm tốc độ 
+
 
 app.get('/products', (req, res) => {
   res.send("<h1>Trang sản phẩm</h1>");
@@ -37,10 +47,13 @@ app.get('/blog', (req, res) => {
   res.send("<h1>Trang blog</h1>");
 });
 
-app.get('/contacts', (req, res) => {
+app.get('/contact', (req, res) => {
   res.send("<h1>Trang liên hệ</h1>");
 });
 
 app.listen(port, () => {
+  /// như thông báo khi chạy server
   console.log(`Example app listening on port ${port}`)
 });
+
+// đã học đc 1h8p
