42502f94778b892fa99280c1cfd54ba8f0c8edb7
DTC174 <a01672034597@gmail.com>
2024-10-10 20:23:33 +0700

    Cấu trúc lại folder PUG

--- product-management/index.js
+++ product-management/index.js
@@ -5,6 +5,7 @@ const route = require("./routes/client/index.route");
 
 const app = express();
 const port = process.env.PORT;
+// đã học 22p
 
 app.set('views', './views');
 app.set('view engine', 'pug');
