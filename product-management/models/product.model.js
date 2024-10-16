f5e4e6120be2a572e2feeec3fe52b366cf14305e
DTC174 <a01672034597@gmail.com>
2024-10-16 21:25:22 +0700

    Làm danh sách sản phẩm bên client

--- product-management/models/product.model.js
+++ product-management/models/product.model.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose');
 
 // Schema như là 1 khung
-const productSchema = new Schema({
-
+const productSchema = new mongoose.Schema({
   title: String,
   description: String,
+  category: String,
   price: Number,
   discountPercentage: Number,
+  rating: Number,
   stock: Number,
   thumbnail: String,
+  deleted: Boolean,
+  position: Number,
+  status: String
 });
 
 const Product = mongoose.model('Product', productSchema, "products");
 // Model là 1 bản ghi: gồm tên model, schema và tên collection
+// học đến 1h40p
 
 module.exports = Product;
