c3aaa6717acf7ecf5507871fbda15159dcea0f96
DTC174 <a01672034597@gmail.com>
2024-10-17 23:19:42 +0700

    1.3 làm giao diện danh sách sản phẩm

--- product-management/routes/admin/index.route.js
+++ product-management/routes/admin/index.route.js
@@ -1,9 +1,12 @@
 const systemConfig = require("../../config/system");
+
 const dashboardRoutes = require("./dashboard.route");
+const productRoutes = require("./product.route");
 
 module.exports = (app) => {
   const PATCH_ADMIN = systemConfig.preFixAdmin;
 
-
   app.use(PATCH_ADMIN + '/dashboard', dashboardRoutes);
+
+  app.use(PATCH_ADMIN + '/products', productRoutes);
 }
