af47d6a8485eed69708c6525fb632c3310c6a72c
DTC174 <a01672034597@gmail.com>
2024-10-22 23:24:56 +0700

    15.1.1. Tối ưu bộ lọc

--- product-management/controllers/admin/product.controller.js
+++ product-management/controllers/admin/product.controller.js
@@ -1,32 +1,9 @@
 const Product = require("../../models/product.model");
+const filterStatusHelper = require("../../helpers/filterStatus");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  let filterStatus = [
-    {
-      name: "Tất cả",
-      status: "",
-      class: ""
-    },
-    {
-      name: "Hoạt động",
-      status: "active",
-      class: ""
-    },
-    {
-      name: "Dừng hoạt động",
-      status: "inactive",
-      class: ""
-    }
-  ];
-
-  if (req.query.status) {
-    const index = filterStatus.findIndex(item => item.status == req.query.status);
-    filterStatus[index].class = "active";
-  } else {
-    const index = filterStatus.findIndex(item => item.status == "");
-    filterStatus[index].class = "active";
-  }
+  filterStatus = filterStatusHelper(req.query);
 
   let find = {
     deleted: false
