b75ec602bd75d8dc9bb60257bc72d45a659e82b3
DTC174 <a01672034597@gmail.com>
2024-10-24 00:35:03 +0700

    nhầm 15 >> 17.2. Tính năng phân trang

--- product-management/public/admin/js/script.js
+++ product-management/public/admin/js/script.js
@@ -37,3 +37,20 @@ if (formSearch) {
   })
 }
 // End FormSearch  
+
+// Pagination
+const buttonsPagination = document.querySelectorAll("[button-pagination]");
+// "[]": dùng để cái tự định nghĩa
+if (buttonsPagination) {
+  let url = new URL(window.location.href);
+  buttonsPagination.forEach(button => {
+    button.addEventListener("click", () => {
+      const page = button.getAttribute("button-pagination")
+      
+      url.searchParams.set("page", page);
+      window.location.href = url.href;
+    })
+  })
+
+}
+// End Pagination
