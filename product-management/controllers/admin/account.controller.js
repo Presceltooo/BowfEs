39642e27ec260a25574903313422381ceb70b4d2
DTC174 <a01672034597@gmail.com>
2025-07-18 17:05:16 +0700

    #21.1.5. Add Account Edit Page

--- product-management/controllers/admin/account.controller.js
+++ product-management/controllers/admin/account.controller.js
@@ -41,7 +41,6 @@ module.exports.index = async (req, res) => {
   });
 }
 
-
 // [GET] /admin/accounts/create
 module.exports.create = async (req, res) => {
   const roles = await Role.find({ deleted: false });
@@ -70,4 +69,66 @@ module.exports.createPost = async (req, res) => {
 
     res.redirect(`${SystemConfig.preFixAdmin}/accounts`);
   }
+}
+
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async (req, res) => {
+  let find = {
+    $and: [{
+      _id: req.params.id
+    },
+    {
+      $or: [{
+        deleted: false
+      }, {
+        deletedAt: {
+          $ne: null
+        }
+      }]
+    }
+    ]
+    };
+  
+  try {
+    const data = await Account.findOne(find);
+
+    const roles = await Role.find({ deleted: false });
+
+  res.render("admin/pages/accounts/edit", {
+    pageTitle: "Chỉnh sửa tài khoản",
+    data: data,
+    roles: roles
+  });
+  } catch (error) {
+    res.redirect(`${SystemConfig.preFixAdmin}/accounts`);
+  }
+}
+
+// [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  const emailExist = await Account.findOne({
+    _id: { $ne: id },
+    email: req.body.email,
+    deleted: false
+  });
+
+  if (emailExist) {
+    req.flash("error", `Email ${req.body.email} đã tồn tại`);
+  } else {
+    if (req.body.password) {
+      req.body.password = md5(req.body.password);
+    } else {
+      delete req.body.password;
+    }
+
+    await Account.updateOne({
+      _id: id
+    }, req.body);
+
+    req.flash("success", "Cập nhật tài khoản thành công!");
+  }
+
+  res.redirect("back");
 }
