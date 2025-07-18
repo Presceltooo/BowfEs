395ac5db2adedee1dca78a75232340518bac7649
DTC174 <a01672034597@gmail.com>
2025-07-18 20:43:56 +0700

    #21.3. Add Logout Page

--- product-management/controllers/admin/auth.controller.js
+++ product-management/controllers/admin/auth.controller.js
@@ -41,3 +41,9 @@ module.exports.loginPost = async (req, res) => {
   res.cookie("token", user.token);
   res.redirect(`${SystemConfig.preFixAdmin}/dashboard`);
 }
+
+// [GET] /admin/auth/logout
+module.exports.logout = async (req, res) => {
+  res.clearCookie("token");
+  res.redirect(`${SystemConfig.preFixAdmin}/auth/login`);
+}
