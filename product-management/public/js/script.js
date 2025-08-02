e7f817a063cacb924041cd91c58ade7191531db5
DTC174 <a01672034597@gmail.com>
2025-08-02 16:46:13 +0700

    #25.2.3.2. Add Button Go Back On Product Detail Page

--- product-management/public/js/script.js
+++ product-management/public/js/script.js
@@ -14,3 +14,14 @@ if (showAlert) {
   });
 }
 // End Show Alert 
+
+// Button Go Back
+const buttonGoBack = document.querySelectorAll("[button-go-back");
+if (buttonGoBack.length > 0) {
+  buttonGoBack.forEach(button => {
+    button.addEventListener("click", () => {
+      history.back();
+    });
+  })
+}
+// End Button Go Back 
