20851923da1fd8fdc46ae22f371ba92e328dbcfa
DTC174 <a01672034597@gmail.com>
2025-08-13 20:08:44 +0700

    #29.1.1. Insert Icon

--- product-management/public/js/chat.js
+++ product-management/public/js/chat.js
@@ -1,6 +1,8 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
 // CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
-console.log(formSendData);
+
 if (formSendData) {
   formSendData.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -48,3 +50,27 @@ if (bodyChat) {
   bodyChat.scrollTop = bodyChat.scrollHeight;
 }
 // End Scroll Chat To Bottom
+
+// emoji-picker
+// Show Popup
+const buttonIcon = document.querySelector(".button-icon");
+if (buttonIcon) {
+  const tooltip = document.querySelector('.tooltip');
+  Popper.createPopper(buttonIcon, tooltip);
+
+  buttonIcon.onclick = () => {
+      tooltip.classList.toggle('shown')
+  };
+}
+
+// Insert Icon to input
+const emojiPicker = document.querySelector("emoji-picker");
+if (emojiPicker) {
+  const inputChat = document.querySelector(".chat .inner-form input[name='content']");
+
+  emojiPicker.addEventListener("emoji-click", (event) => {
+    const icon = event.detail.unicode;
+    inputChat.value = inputChat.value + icon;
+  });
+}
+// End emoji-picker
