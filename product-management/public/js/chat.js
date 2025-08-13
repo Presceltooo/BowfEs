376ead98157d3d4e5c49e6adbc08ad45a5a92496
DTC174 <a01672034597@gmail.com>
2025-08-13 22:25:04 +0700

    #29.1.2. Typing

--- product-management/public/js/chat.js
+++ product-management/public/js/chat.js
@@ -12,6 +12,7 @@ if (formSendData) {
     if (content) {
       socket.emit("CLIENT_SEND_MESSAGE", content);
       e.target.elements.content.value = "";
+      socket.emit("CLIENT_SEND_TYPING", "hidden");
     }
   });
 }
@@ -21,6 +22,7 @@ if (formSendData) {
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
   const myId = document.querySelector("[my-id]").getAttribute("my-id");
   const body = document.querySelector(".chat .inner-body");
+  const boxTyping = document.querySelector(".inner-list-typing");
 
   const div = document.createElement("div");
   
@@ -38,9 +40,9 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
   <div class="inner-content">${data.content}</div>
   `;
 
-  body.appendChild(div);
+  body.insertBefore(div, boxTyping);
 
-  bodyChat.scrollTop = bodyChat.scrollHeight;
+  body.scrollTop = body.scrollHeight;
 })
 // END SERVER_RETURN_MESSAGE
 
@@ -63,6 +65,20 @@ if (buttonIcon) {
   };
 }
 
+// Show Typing
+var timeOut;
+
+const showTyping = () => {
+  socket.emit("CLIENT_SEND_TYPING", "show");
+
+  clearTimeout(timeOut);
+
+  timeOut = setTimeout(() => {
+    socket.emit("CLIENT_SEND_TYPING", "hidden");
+  }, 3000);
+}
+// End Show Typing
+
 // Insert Icon to input
 const emojiPicker = document.querySelector("emoji-picker");
 if (emojiPicker) {
@@ -71,6 +87,50 @@ if (emojiPicker) {
   emojiPicker.addEventListener("emoji-click", (event) => {
     const icon = event.detail.unicode;
     inputChat.value = inputChat.value + icon;
+    const end = inputChat.value.length;
+    inputChat.setSelectionRange(end, end);
+    inputChat.focus();
+
+    showTyping();
+  }); 
+
+  inputChat.addEventListener("keyup", () =>{
+    showTyping();
   });
 }
 // End emoji-picker
+
+// SERVER_RETURN_TYPING
+const elementListTyping = document.querySelector(".chat .inner-list-typing");
+if (elementListTyping) {
+  socket.on("SERVER_RETURN_TYPING", (data) => {
+    if (data.type == "show") {
+      const existTyping = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+
+      if (!existTyping) {
+        const boxTyping = document.createElement("div");
+        const body = document.querySelector(".chat .inner-body");
+        boxTyping.classList.add("box-typing");
+        boxTyping.setAttribute("user-id", data.userId);
+
+        boxTyping.innerHTML = `
+          <div class="inner-name">${data.fullName}</div>
+          <div class="inner-dots"></div>
+            <span></span>
+            <span></span>
+            <span></span>
+        `;
+
+        elementListTyping.appendChild(boxTyping);
+
+        body.scrollTop = body.scrollHeight;
+      }
+    } else {
+      const boxTypingRemove = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+      if (boxTypingRemove) {
+        elementListTyping.removeChild(boxTypingRemove);
+      }
+    }
+  });
+}
+// End SERVER_RETURN_TYPING
